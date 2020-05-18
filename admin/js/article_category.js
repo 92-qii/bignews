739afc605118e245e5d5ca3da8dd87b04bc1cfb5
92_qii <497370665@qq.com>
2020-05-18 13:19:27 +0800

    文章分类的增删改功能完成

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -163,27 +163,122 @@ $(function () {
 
   // 1. 发送请求获取数据，渲染页面
   // 1.1 发送ajax请求
-  $.ajax({
-    type: 'get',
-    url: BigNew.category_list,
-    success: function (res) {
-      console.log(res);//获取内容如下：
-      /*code: 200
-     data: Array(5)
-     0: {id: 1, name: "爱生活", slug: "热爱生活"}
-     1: {id: 2, name: "爱旅行", slug: "热爱旅行"}
-     2: {id: 3, name: "爱美食", slug: "热爱美食"}
-     3: {id: 4, name: "爱运动", slug: "热爱运动"}
-     4: {id: 5, name: "经济特区", slug: "热爱经济"}
-     length: 5
-     __proto__: Array(0)
-     msg: "获取成功"*/
-      console.log(typeof res);
-      // 1.2把数据通过模板引擎渲染到页面上
-      if (res.code == 200) {
-        var htmlStr = template('categoryList', res)
-        $('tbody').html(htmlStr)
+  render()
+  function render() {
+    $.ajax({
+      type: 'get',
+      url: BigNew.category_list,
+      success: function (res) {
+        console.log(res);//获取内容如下：
+        /*code: 200
+       data: Array(5)
+       0: {id: 1, name: "爱生活", slug: "热爱生活"}
+       1: {id: 2, name: "爱旅行", slug: "热爱旅行"}
+       2: {id: 3, name: "爱美食", slug: "热爱美食"}
+       3: {id: 4, name: "爱运动", slug: "热爱运动"}
+       4: {id: 5, name: "经济特区", slug: "热爱经济"}
+       length: 5
+       __proto__: Array(0)
+       msg: "获取成功"*/
+        console.log(typeof res);
+        // 1.2把数据通过模板引擎渲染到页面上
+        if (res.code == 200) {
+          var htmlStr = template('categoryList', res)
+          $('tbody').html(htmlStr)
+        }
       }
-    }
+    })
+  }
+
+  // 2.给新增按钮注册点击事件
+  $("#xinzengfenlei").on("click", function () {
+    // alert("123")
+    // 2.1显示模态框
+    $(".add_modal").modal("show")
+    // 2.2修改标题
+    $(".add_modal  h4").text("新增分类")
+    // 2.3清空模态框
+    $('#myForm')[0].reset()  //DOM对象的表单重置
   })
+
+
+  // 3.给模态框中的编辑键添加事件(以委托形式)
+  $("tbody").on("click", ".btn-edit", function () {
+    // alert("123")
+
+    // 3.1显示模态框
+    $(".add_modal").modal("show")
+    // 3.2修改标题
+    $(".add_modal  h4").text("编辑分类")
+    // 3.3获取当前按钮所在的那条数据的索引值 
+    var id = $(this).data('id')
+
+    // 3.4发送ajax请求，将获取的数据渲染到模态框中
+    $.ajax({
+      type: 'get',
+      url: BigNew.category_search,
+      data: {
+        id: id
+      },
+      success: function (res) {
+        if (res.code == 200) {
+          $('#myForm input[name=id]').val(res.data[0].id)
+          $('#myForm input[name=name]').val(res.data[0].name)
+          $('#myForm input[name=slug]').val(res.data[0].slug)
+        }
+      }
+    })
+  })
+
+  // 4.给模态框中的确定按钮注册事件
+  $(".add_modal .btn-sure").on("click", function () {
+    // 4.1通过判断是否有id来判断当前操作是更新还是添加
+    // 获取隐藏域中的id
+    var id = $('#myForm input[name=id]').val()
+    // 4.2发送ajax请求
+    $.ajax({
+      type: 'post',
+      // 点击确定按钮判断有没有id，如果有就是更新没有就是添加
+      url: id ? BigNew.category_edit : BigNew.category_add,
+      // 4.3收集表单中所有信息
+      data: $('#myForm').serialize(),
+      success: function (res) {
+        // console.log(res);
+        if (res.code == 200 || res.code == 201) {
+          //隐藏模态框
+          $('.add_modal').modal('hide');
+          // 刷新当前页面
+          render()
+        }
+      }
+    })
+  })
+
+
+  // 5.给模态框中的删除按钮注册事件(委托事件)
+  $('tbody').on("click", '.btn-del', function () {
+    // 5.1先弹出模态框
+    $('.delModal').modal('show');
+    // 5.2获取当前按钮所在的那条数据的id
+    window.categoryId = $(this).data('id')
+    // console.log(window.categoryId)
+  })
+  // 给删除模态框确定按钮注册事件
+  $('.delModal .btn-sure').on('click', function () {
+    $.ajax({
+      type: 'post',
+      url: BigNew.category_delete,
+      data: {
+        id: window.categoryId
+      },
+      success: function (res) {
+        if (res.code == 204) {
+          $('.delModal').modal('hide');
+          render()
+        }
+      }
+    })
+  })
+
+
 })
